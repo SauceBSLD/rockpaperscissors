077770a63ba10fb0e55a8a0c4d7f8e2880be4428
SauceBSLD <saucebosssbmusic@gmail.com>
2022-09-27 16:42:07 +0200

    Finished the game

---
+++ script.js
@@ -0,0 +1,39 @@
+function getComputerChoice() {
+    let choice = ["Rock", "Paper", "Scissors"];
+    let computerSelection = choice[Math.floor(Math.random() * choice.length)];
+    return computerSelection;
+}
+
+function getPlayerChoice(input) {
+    choice = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+    if (choice === "Rock" || choice === "Paper" || choice === "Scissors") {
+        let computerSelection = getComputerChoice();
+        playerSelection = choice;
+        playRound(computerSelection, playerSelection);
+    } else {
+        console.log("You need to choose between one of the three choices!");
+        startGame();
+    }
+}
+
+function playRound(compChoice, playerChoice) {
+    console.log(`You used the ${playerChoice}`);
+    console.log(`Your opponent uses the ${compChoice}`);
+    if (playerChoice === compChoice) {
+        console.log("You're even! Try again.");
+        startGame();
+    } else if ((playerChoice === "Rock" && compChoice === "Scissors") || (playerChoice === "Paper" && compChoice === "Rock")
+    || (playerChoice === "Scissors" && compChoice === "Rock")) {
+        console.log("You won this round!");
+        confirm("Do you want to play again?") ? startGame() : console.log("Game ended");
+    } else {
+        console.log("Fuck! You lost...");
+        confirm("Do you want to play again?") ? startGame() : console.log("Game ended");
+    }
+}
+
+function startGame() {
+    confirm("Do you want to start a new game?") ? getPlayerChoice(prompt("Rock, Paper or Scissors ?", "")) : console.log("See you later!");
+}
+
+startGame()
